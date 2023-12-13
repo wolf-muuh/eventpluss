7f0f2be509dd84f122d908cd605b8ef0be41ae62
Murilo Alves Zapiello <murilo.zapiello@gmail.com>
2023-12-13 17:18:35 -0300

    inicio da pagina de detalhes

---
+++ src/pages/DetalhesPage/Detalhes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import MainContent from "../../components/MainContent/MainContent";
+import Title from "../../components/Title/Title";
+import Container from "../../components/Container/Container";
+
+const Detalhes = () => {
+  return (
+    <>
+      <MainContent>
+        <Container>
+          <Title titleText={"Detalhes do evento"} />
+          
+        </Container>
+      </MainContent>
+    </>
+  );
+};
+
+export default Detalhes;
